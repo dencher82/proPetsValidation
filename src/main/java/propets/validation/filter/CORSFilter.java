57ab1d5159be0abed17cfaa25b1ec3ad88ea4309
den_z <den_z@DESKTOP-4HE61H0>
2020-11-18 15:23:56 +0200

    Implement CORS

--- src/main/java/propets/validation/filter/CORSFilter.java
+++ src/main/java/propets/validation/filter/CORSFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -17,11 +18,14 @@ import org.springframework.stereotype.Component;
 @Order(5)
 public class CORSFilter implements Filter {
 
+	@Value("${origin.url}")
+	private String originUrl;
+	
 	@Override
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		HttpServletRequest request = (HttpServletRequest) req;
-		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Origin", originUrl);
 		response.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
 		response.setHeader("Access-Control-Max-Age", "3600");
 		response.setHeader("Access-Control-Allow-Headers", "X-Token, content-type, Authorization");
